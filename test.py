3d1c4ff7495b747f3209325233fc84c3bb81c673
Gabwabie <gabwabie@gmail.com>
2025-02-05 18:44:59 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+import keyword
+
+
+first_name = "Gabby"
+age = 14
+height = 5.2
+
+print("My name is Gabby \n I like coding")
+print("My name is ",first_name)
+print("My favourite number is ", 72)
+print("My favourite shape is ", end="*")
+
+num1 = float(input("Enter num1: "))
+num2 = input("Enter num2: ")
+num2 = int(num2)
+print(num1 + num2)
+
+#str() = string
+#float() = float
+#int() = integer
+
+print("Python keywords are... \n")
+print(keyword.kwlist)
