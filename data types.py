c8c8d4604e0ae556337ec08a419583901d6ca8cc
Gabwabie <gabwabie@gmail.com>
2025-02-09 16:53:21 -0400

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,20 @@
+#num1 = input("Type a number")
+#num2 = input("Type a number")
+#num3 = input("Type a number")
+#print(int(int(num1) + int(num2) + int(num3)))
+#print(str(num1 + num2 + num3))
+#print(float(num1 + num2 + num3))
+
+#name = input("Please enter your name")
+# age = input("Please enter your age")
+# contact = input("Please enter your phone number")
+# email = input("Please enter your email")
+# address = input("Please enter your address")
+# print("Your name is ", name, "your age is ", age, "your phone number is ", contact, "your email is", email,
+#       "and your address is ", address, end=".")
+
+username = "string"
+# print(username[2::1])
+username.upper()
+print(username)
+
