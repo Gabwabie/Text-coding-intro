ba518ff62dc45680e5eaa59d4d903a87c843a3b2
Gabwabie <gabwabie@gmail.com>
2025-02-16 16:45:04 -0400

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,28 @@
+# age = int(input("Enter age: "))
+# if age >=0 and age <=5:
+#     print("infant")
+# actual_cost = float(input("Please Enter the Actual Product Price: "))
+# sale_amount = float(input("Please Enter the Sales Amount: "))
+
+# if (sale_amount > actual_cost):
+#     amount = sale_amount - actual_cost
+#     print("Total profit = {amount}")
+# else:
+#     print("No profit!!!")
+
+# i = int(input("enter a number: "))
+# if (i < 15):
+#     print("i is smaller than 15")
+#     print("Im in if Block")
+# else:
+#     print("i is greater than 15")
+#     print("im in else Block")
+# print("Im not in if and not in else Block")
+
+number = int(input("Enter Number to check"))
+print("Number to be checked: ", number)
+
+if number%2==0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
