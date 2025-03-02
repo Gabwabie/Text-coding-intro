96bea84054084a2933c5233220e8984881c205fb
Gabwabie <gabwabie@gmail.com>
2025-03-02 16:44:05 -0400

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,45 @@
+#Sum of natural numbers
+# n = int(input("Enter the value of terms: "))
+# sum = 0
+# i = 1
+# while i<=n:
+#     sum = sum+i
+#     i = i+1
+# print("\nSum = ",sum)
+
+#Infinity
+# while i<=0:
+#     print("I WILL RUN FOREVER")
+
+#
+# num = int(input("Enter a number: "))
+# sum = 0
+# temp = num
+
+# while temp > 0:
+#     digit = temp % 10
+#     sum += digit ** 3
+#     temp //= 10
+
+# if num == sum:
+#     print(num, "is an Armstrong number")
+# else:
+#     print(num, "is not an Armstrong number")
+
+#
+i = 1
+while i < 11:
+    print(i)
+    if i ==6:
+        break
+    i = i + 1
+
+print("\n")
+
+i = 0
+while i < 10:
+    i = i + 1
+
+    if i ==6:
+        continue
+    print(i)
